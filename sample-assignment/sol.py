c423a0f5d8f988cd2eb32bf204952bc468273daa
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-02 11:32:55 +0530

    " -> 1 of 1 passed. pylint: 10/10 "

--- sample-assignment/sol.py
+++ sample-assignment/sol.py
@@ -4,11 +4,11 @@ Write a Python function, factorial(n), that takes in one number and
 returns the factorial of given number.
 """
 def main():
-	"""
-	This function takes in one number and returns one number.
-	"""
-	data = input()
-	print(int(data[0])+int(data[1]))
+    """
+    This function takes in one number and returns one number.
+    """
+    data = input()
+    print(int(data[0])+int(data[1]))
 
 if __name__ == "__main__":
-	main()
+    main()

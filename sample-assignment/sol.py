1bdb0c7ccc9ea1eff37759fc89091fc8c91ca4e8
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-02 11:32:39 +0530

    " -> 1 of 1 passed. pylint: 2/10 "

--- sample-assignment/sol.py
+++ sample-assignment/sol.py
@@ -7,8 +7,8 @@ def main():
 	"""
 	This function takes in one number and returns one number.
 	"""
-    data = input()
-    print(int(data[0])+int(data[1]))
+	data = input()
+	print(int(data[0])+int(data[1]))
 
 if __name__ == "__main__":
-    main()
+	main()

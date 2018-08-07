fb4608bb7100749bdf47bc2e914d2880f2b7369b
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-07 15:09:13 +0530

    string rec ex final commit

--- m8/Is In Exercise/is_in.py
+++ m8/Is In Exercise/is_in.py
@@ -3,34 +3,35 @@ Author: Prudhvik Chirunomula
 Date: 07-08-2018
 '''
 # Exercise: Is In
-# Write a Python function, isIn(char, aStr), that takes in two arguments
-# a character and String and returns the isIn(char, aStr) which retuns a
+# Write a Python function, is_in(char, a_str), that takes in two arguments
+# a character and String and returns the is_in(char, a_str) which retuns a
 # boolean value.
 
 # This function takes in two arguments character and String and returns one boolean value.
 
-def isIn(char, aStr):
-	'''
-	char: a single character
-	aStr: an alphabetized string
-	
-	returns: True if char is in aStr; False otherwise
-	'''
-	# Your code here
-	len_str = len(aStr)
-	if len_str == 0:
-		return False
-	if aStr[len_str//2] == char:
-		return True
-	elif aStr[len_str//2] > char:
-		return isIn(char, aStr[0:len_str//2])
-	return isIn(char,a[len_str//2:])
+def is_in(char, a_str):
+    '''
+    char: a single character
+    a_str: an alphabetized string
+
+    returns: True if char is in a_str; False otherwise
+    '''
+    # Your code here
+    len_str = len(a_str)
+    if len_str == 0:
+        return False
+    if a_str[len_str//2] == char:
+        return True
+    elif a_str[len_str//2] > char:
+        return is_in(char, a_str[0:len_str//2])
+    return is_in(char, a_str[len_str//2:])
 
 def main():
-	data = input()
-	data = data.split()
-	print(isIn((data[0][0]), data[1]))
+    '''main function'''
+    data = input()
+    data = data.split()
+    print(is_in((data[0][0]), data[1]))
 
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()

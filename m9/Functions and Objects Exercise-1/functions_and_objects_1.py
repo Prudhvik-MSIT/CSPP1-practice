ac4b7c22da8899870cd9db02d1de08e428dbd2db
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-08 15:06:11 +0530

    function and objects -1 final copy

--- m9/Functions and Objects Exercise-1/functions_and_objects_1.py
+++ m9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -1,17 +1,32 @@
+'''
+Author: Prudhvik Chirunomula
+Date: 08-08-2018
+'''
 #Exercise : Function and Objects Exercise-1
-#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+#Implement a function that converts the given testList = [1, -4, 8, -9]
+# into [1, 4, 8, 9]
 
 
-def apply_to_each(L, f):
-    
+def apply_to_each(list_to_be_modified, function_to_be_applied):
+    '''
+    function that converts the given testList = [1, -4, 8, -9] into
+    [1, 4, 8, 9]
+    '''
+    for iter_val, _ in enumerate(list_to_be_modified):
+        list_to_be_modified[iter_val] = function_to_be_applied(
+            list_to_be_modified[iter_val])
 
 def main():
+    '''
+    main function
+    '''
     data = input()
     data = data.split()
     list1 = []
-    for j in l:
+    for j in data:
         list1.append(int(j))
     apply_to_each(list1, abs)
+    print(list1)
 
 if __name__ == "__main__":
     main()

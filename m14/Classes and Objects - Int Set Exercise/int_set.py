db2932f8de7d80dec17da9206fe1a86c4620fcbe
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-21 18:28:39 +0530

    intset complete

--- m14/Classes and Objects - Int Set Exercise/int_set.py
+++ m14/Classes and Objects - Int Set Exercise/int_set.py
@@ -36,6 +36,11 @@ class intSet(object):
         """Returns a string representation of self"""
         self.vals.sort()
         return '{' + ','.join([str(e) for e in self.vals]) + '}'
+    
+    def __repr__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
         
 # Your task is to define the following two methods for the intSet class:
 #   1. Define an intersect method that returns a new intSet containing elements that appear in both sets. In other words,
@@ -46,9 +51,9 @@ class intSet(object):
     def intersect(self, s2):
         newList = intSet()
         for i in self.vals:
-            if i in s2:
+            if i in s2.vals:
                 newList.insert(i)
-        return intSet
+        return newList
 
 
 #   2. Add the appropriate method(s) so that len(s) returns the number of elements in s.
@@ -62,15 +67,16 @@ class intSet(object):
 def main():
     setA = intSet()
     setB = intSet()
-    data=raw_input()
-    data1=raw_input()
+    data=input()
+    data1=input()
     l1=data.split()
     l2=data1.split()
     for i in l1:
         setA.insert(int(i))
     for j in l2:
         setB.insert(int(j))
-    print setA.intersect(setB)
+    # print(setA,setB)
+    print(setA.intersect(setB))
 
 if __name__== "__main__":
     main()
